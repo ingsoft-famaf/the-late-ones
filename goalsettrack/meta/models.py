e517cf599d70f0a74cb8475bbee9db659652b5db
Jeremías Feltan <jerefeltan@gmail.com>
2016-10-20 11:37:27 -0300

    Agregué fecha de modificación a metas. Clase abstracta.

--- goalsettrack/meta/models.py
+++ goalsettrack/meta/models.py
@@ -34,18 +34,16 @@ class MetaAbstracta(models.Model):
         (F, 'F'),
     )
 
+    abstract = True
     titulo = models.CharField(max_length=80)
     descripcion = models.CharField(max_length=1000)
     estado = models.CharField(max_length=15, choices=ESTADOS)
     prioridad = models.CharField(max_length=15, choices=PRIORIDADES)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
     fecha_comienzo = models.DateTimeField('comienzo')
     fecha_fin = models.DateTimeField('fin')
     fecha_vencimiento = models.DateTimeField('vencimiento')
-
-    def modificar(self):
-        """ Modificar los atributos """
-        pass
 
 
 class Meta(MetaAbstracta):
