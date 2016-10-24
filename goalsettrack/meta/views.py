630300d66e153136cdaa3f43fd72a700d5a2784b
Miguel Maldonado <miguel.alberto.maldonado@gmail.com>
2016-10-24 18:58:56 -0300

    Se agregan vistas

--- goalsettrack/meta/views.py
+++ goalsettrack/meta/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def crear_meta(request):
-    return HttpResponse("Crear nueva meta.")
+    return render(request, 'crear_meta.html')
+
+
+def lista_de_metas(request):
+    return render(request, 'lista_de_metas.html')
