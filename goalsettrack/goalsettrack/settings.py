71ce1c428d8685d1ebdbc6f04ae7b4cf1583c31b
Miguel Maldonado <miguel.alberto.maldonado@gmail.com>
2016-10-24 18:56:24 -0300

    Se agrega path para los templates

--- goalsettrack/goalsettrack/settings.py
+++ goalsettrack/goalsettrack/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'goalsettrack.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(os.path.join(BASE_DIR,'templates'), 'meta')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
