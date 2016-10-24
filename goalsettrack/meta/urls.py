97df9f36ebd5465996cddae6e1228ec4dd185441
Miguel Maldonado <miguel.alberto.maldonado@gmail.com>
2016-10-24 19:07:49 -0300

    Se configura URLs de las vistas

--- goalsettrack/meta/urls.py
+++ goalsettrack/meta/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.crear_meta, name='crear_meta'),
+    url(r'^lista_de_metas/$', views.lista_de_metas, name='lista_de_metas'),
+    url(r'^lista_de_metas/crear_meta/$', views.crear_meta, name='crear_meta'),
 ]
