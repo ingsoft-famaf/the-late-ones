5fd0fb25391a7bbfc32e2630562acc92b7119c1a
Juan Costamagna <juancostamagna1986@gmail.com>
2016-10-24 18:02:46 -0300

    agregado de las vistas de login logout

--- goalsettrack/usuario/urls.py
+++ goalsettrack/usuario/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
-
-from . import views
+from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
+from .views import *
+from django.contrib.auth.views import logout
 
 urlpatterns = [
     # pagina principal
-    url(r'^$', views.main_page, name=''),
-    # pagina de registro
-    url(r'^$', views.pagina_registro, name='registro'),
+    url(r'^$', views.main_page, name='/'),
+    # pagina de login
+    url(r'^$/login/', TemplateView.as_view(template_name='login.html')),
+    # pagina de logout
+    url(r'^logout$',logout, name="logout"),
 ]
