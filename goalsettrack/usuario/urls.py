f17bbfec6af9e289f816fbb7b386e3ff70e31e12
Jeremías Feltan <jerefeltan@gmail.com>
2016-10-25 10:54:21 -0300

    corrigiendo registro usuario

--- goalsettrack/usuario/urls.py
+++ goalsettrack/usuario/urls.py
@@ -1,17 +1,15 @@
+""" Parseo de urls de la app usuario """
+
 from django.conf.urls import url
-from django.contrib.auth import views as auth_views
+# from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
-from . import views
 from django.contrib.auth.views import logout
+from . import views
 
 
 urlpatterns = [
-    # pagina principal
     url(r'^$', views.main_page, name='/'),
-    # pagina de login
-    url(r'^$/login/', TemplateView.as_view(template_name='login.html')),
-    # pagina de logout
+    url(r'^login$', TemplateView.as_view(template_name='login.html')),
     url(r'^logout$', logout, name="logout"),
-    # pagina de registro
     url(r'^registro', views.Registro.as_view(), name='registro'),
 ]
--- goalsettrack/usuario/views.py
+++ goalsettrack/usuario/views.py
@@ -1,29 +1,21 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.urls import reverse
-from django.views import generic
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
-from django.template import Context
-from django.template.loader import get_template
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User, Permission
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.auth import views
-from django.views.generic import CreateView, UpdateView, ListView
-from django.views.generic import DetailView, TemplateView
-from django.shortcuts import get_object_or_404
+""" Vistas de la aplicación usuario """
+
 from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.views.generic import CreateView
+
+
 
 def main_page(request):
+    """ Opciones: ir a registrarse o a iniciar sesión """
     template = get_template('main_page.html')
     output = template.render()
     return HttpResponse(output)
 
-class Registro(CreateView):
 
+class Registro(CreateView):
+    """ Automático de Django """
     template_name = 'registro.html'
     form_class = UserCreationForm
     success_url = '/login/'
