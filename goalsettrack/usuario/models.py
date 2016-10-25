cd2600ea32784e19f64813114ece55da1d3458d4
Jeremías Feltan <jerefeltan@gmail.com>
2016-10-25 10:54:21 -0300

    corrigiendo registro usuario

--- goalsettrack/usuario/models.py
+++ goalsettrack/usuario/models.py
@@ -5,6 +5,7 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.core.validators import MaxValueValidator
 
+
 class Usuario(models.Model):
     usuario = models.OneToOneField(User, null=True)
     nombre = models.CharField(max_length=30, blank=True)
