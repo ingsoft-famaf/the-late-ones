1e22c5267e491aca064a4605a6e608b61bd446f6
Miguel Maldonado <miguel.alberto.maldonado@gmail.com>
2016-10-26 16:54:42 -0300

    Se agregan restricciones de integradad y relaciones de meta

--- goalsettrack/meta/models.py
+++ goalsettrack/meta/models.py
@@ -1,6 +1,7 @@
 """ Tablas de la base de datos sobre Metas """
 
 from django.db import models
+from usuario.models import Usuario
 
 
 class MetaAbstracta(models.Model):
@@ -35,18 +36,32 @@ class MetaAbstracta(models.Model):
     )
 
     abstract = True
-    titulo = models.CharField(max_length=80)
-    descripcion = models.CharField(max_length=1000)
-    estado = models.CharField(max_length=15, choices=ESTADOS)
-    prioridad = models.CharField(max_length=15, choices=PRIORIDADES)
+    titulo = models.CharField(max_length=80, default='TITULO META')
+    descripcion = models.CharField(max_length=1000, default='Descripcion.')
+    estado = models.CharField(
+        max_length=15, choices=ESTADOS, default=PENDIENTE)
+    prioridad = models.CharField(max_length=15, choices=PRIORIDADES, default=A)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     fecha_modificacion = models.DateTimeField(auto_now=True)
-    fecha_comienzo = models.DateTimeField('comienzo')
-    fecha_fin = models.DateTimeField('fin')
-    fecha_vencimiento = models.DateTimeField('vencimiento')
+    fecha_comienzo = models.DateTimeField('comienzo', null=True, blank=True)
+    fecha_fin = models.DateTimeField('fin', null=True, blank=True)
+    fecha_vencimiento = models.DateTimeField(
+        'vencimiento', null=True, blank=True)
 
 
 class Meta(MetaAbstracta):
+    """
+    Una relación muchos-a-uno. Requiere un argumento posicional: la clase a
+    la que se relaciona el modelo.
+    """
+
+    # An object that has a many-to-one relationship
+    # many 'Meta' to one 'Usuario'
+    user = models.ForeignKey('Usuario', on_delete=models.CASCADE)
+
+
+class Submeta(MetaAbstracta):
     """ Hereda de MetaAbstracta """
 
-    pass
+    # many 'Submetas' to one 'Meta'
+    meta = models.ForeignKey('Meta', on_delete=models.CASCADE)
