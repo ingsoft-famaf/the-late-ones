d91e134f49b99a32f6c311fa718f1208ae15b009
Jeremías Feltan <jerefeltan@gmail.com>
2016-10-26 18:35:55 -0300

    Agregué view para crear submeta. Corregí estilo de templates de meta

--- goalsettrack/meta/urls.py
+++ goalsettrack/meta/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^lista_de_metas/$', views.lista_de_metas, name='lista_de_metas'),
     url(r'^lista_de_metas/crear_meta/$', views.crear_meta, name='crear_meta'),
-    url(r'^lista_de_metas/crear_meta/crear_submeta$', views.crear_submeta, name='crear_submeta'),
+    url(r'^lista_de_metas/crear_meta/crear_submeta$', views.crear_submeta,
+        name='crear_submeta'),
 ]
--- goalsettrack/meta/views.py
+++ goalsettrack/meta/views.py
@@ -1,10 +1,11 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
 
+def lista_de_metas(request):
+    return render(request, 'lista_de_metas.html')
 
 def crear_meta(request):
     return render(request, 'crear_meta.html')
 
-
-def lista_de_metas(request):
-    return render(request, 'lista_de_metas.html')
+def crear_submeta(request):
+    return render(request, 'crear_submeta.html')
