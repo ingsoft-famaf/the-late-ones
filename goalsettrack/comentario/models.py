cc90309dd9267c3bb730daedb66bd30a71d5e5db
Jeremías Feltan <jerefeltan@gmail.com>
2016-10-27 12:38:57 -0300

    Corregí modelo de comentario

--- goalsettrack/comentario/models.py
+++ goalsettrack/comentario/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from meta.models import MetaAbstracta
 
-# Create your models here.
+
 class Comentario(models.Model):
-    """ Una meta puede tener muchos comentarios, un comentario puede estar en una sola meta"""
-	texto = models.CharField(max_length=1000, default='Comentario')
-	fecha_creacion = models.DateTimeField(auto_now_add=True)
+    """ cada comentarios es de una única meta / submeta """
+    texto = models.CharField(max_length=1000, default='Comentario.')
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
     meta = models.ForeignKey(MetaAbstracta, on_delete=models.CASCADE)
