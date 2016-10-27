1f91cbe73950bd50b06cb259717e462df9454535
IgnaMade <ignamadevollen@gmail.com>
2016-10-27 11:53:08 -0300

    Relaciones de integridad de Clase Comentario finalizadas

--- goalsettrack/comentario/models.py
+++ goalsettrack/comentario/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Comentario(models.Model):
+    """ Una meta puede tener muchos comentarios, un comentario puede estar en una sola meta"""
 	texto = models.CharField(max_length=1000, default='Comentario')
 	fecha_creacion = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(MetaAbstracta, on_delete=models.CASCADE)
