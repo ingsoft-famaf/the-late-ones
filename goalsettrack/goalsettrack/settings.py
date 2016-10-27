09438a26c7e8fc779671103a6009fb00eae7e023
Miguel Maldonado <miguel.alberto.maldonado@gmail.com>
2016-10-27 02:02:48 -0300

    Se configura y agregan directorios de archivos estaticos

--- goalsettrack/goalsettrack/settings.py
+++ goalsettrack/goalsettrack/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'goalsettrack.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['templates',
+            os.path.join(os.path.join(BASE_DIR,'templates'), 'meta'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,4 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
