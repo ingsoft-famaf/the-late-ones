d3ba44b433c04722c27e85df179019ef00273b61
Jeremías Feltan <jerefeltan@gmail.com>
2016-10-26 21:17:43 -0300

    Renombré pagina_principal.html. Corregí archivos de usuario

--- goalsettrack/goalsettrack/urls.py
+++ goalsettrack/goalsettrack/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
-from django.contrib import admin
 # from django.contrib.auth import views as auth_views
 # from django.core.urlresolvers import reverse_lazy
 from django.conf import settings
+from django.conf.urls import include, url
+from django.contrib import admin
 
 
 urlpatterns = [
--- goalsettrack/usuario/urls.py
+++ goalsettrack/usuario/urls.py
@@ -1,22 +1,15 @@
+from . import views
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-from django.views.generic import TemplateView
-from . import views
 from django.contrib.auth.views import logout
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    # pagina principal
-    url(r'^$', views.main_page, name='/'),
-    # pagina de login
+    url(r'^$', views.pagina_principal, name='/'),
     url(r'^$/login/', TemplateView.as_view(template_name='login.html')),
-    # pagina de logout
     url(r'^logout$', logout, name="logout"),
-    # pagina de registro
     url(r'^registro', views.Registro.as_view(), name='registro'),
-    # pagina de edicion de perfil
     url(r'^editar_perfil/', views.EditarPerfil.as_view(), name='editar_perfil'),
-    # pagina de  detalles de perfil
     url(r'^perfil/', views.DetallePerfil.as_view(), name='perfil'),
-    url(r'^lista_de_metas', views.lista_de_metas, name='lista_de_metas'),
 ]
