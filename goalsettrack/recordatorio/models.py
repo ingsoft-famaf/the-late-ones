0155d6bf2fbc2fbd5cff097a75868c1796880873
IgnaMade <ignamadevollen@gmail.com>
2016-10-27 11:56:27 -0300

    Relaciones de integridad de Recordatorio finalizadas

--- goalsettrack/recordatorio/models.py
+++ goalsettrack/recordatorio/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 
 class Recordatorio(models.Model):
+    """ Una meta puede tener muchos recordatorios, un comentario puede estar en una sola meta"""
     titulo = models.CharField(max_length=80, default='Título')
     fecha = models.DateTimeField()
     hora = models.TimeField(null=True, blank=True)
     repetir_cada = models.IntegerField(default=0)
     mensaje = models.CharField(max_length=1000, default='Mensaje')
+    user = models.ForeignKey(MetaAbstracta, on_delete=models.CASCADE)
