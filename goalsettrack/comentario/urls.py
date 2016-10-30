07413244394dbdd83d82172387e39c5d49199780
IgnaMade <igna_made@hotmail.com>
2016-10-30 18:31:34 -0300

    error en urls de comentaio corregido

--- goalsettrack/comentario/urls.py
+++ goalsettrack/comentario/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^crear_comentario/$', views.crear_comentario, name='crear_comentario'),
-    url(r'^detalle_comentario/$', views.crear_comentario, name='detalle_comentario')
+    url(r'^detalle_comentario/$', views.detalle_comentario, name='detalle_comentario')
 ]
 
