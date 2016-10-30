9eede34ea6b5a5fb1381d6191c9a82b5822b23fe
IgnaMade <igna_made@hotmail.com>
2016-10-30 01:30:34 -0300

    error en urls comentario corregido

--- goalsettrack/comentario/urls.py
+++ goalsettrack/comentario/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^crear_comentario/$', views.crear_meta, name='crear_comentario')
+    url(r'^crear_comentario/$', views.crear_comentario, name='crear_comentario')
 ]
 
