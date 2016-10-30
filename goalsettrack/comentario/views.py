32fd05c9c2e05442b82a63f2cad37f8a791d458e
IgnaMade <igna_made@hotmail.com>
2016-10-30 16:45:22 -0300

    funcionalidad de eliminar comentario agregada

--- goalsettrack/comentario/views.py
+++ goalsettrack/comentario/views.py
@@ -20,6 +20,7 @@ def crear_comentario(request, meta_id):
         	# se crea el comentario con los datos del formulario
             comentario = form.save(commit=False)
             # se lo relaciona con la meta (foreing key y eso)
+            # probablemente esto no anda, se deberia hacer una consulta sql y usar lo de las filiminas
             comentario.meta = meta
             # se guarda el comentario en la base de datos
             comentario.save()
@@ -38,4 +39,15 @@ def detalle_comentario(request, comentario_id):
     comentario = get_object_or_404(Comentario, pk=comentario_id)
     return render(request, 'comentario/detalle_comentario.html', {'comentario': comentario})
 
-    
+# se elimina un comentario
+# desde la view de meta usuario hace ckick en eliminar comentario, que pasa
+# el id de tal comentairo a esta funcion, esta funcion obtiene de la base de datos
+# tal comentario a partir del id pasado, obtiene la meta en la que esta a traves de
+# la foreign key, elimina el comentario de la meta y redirecciona al usuario para 
+# el detalle de las metas
+def eliminar_comentario(request, comentario_id):
+    comentario = get_object_or_404(Comentario, pk=comentario_id)
+    # probablemente esto no anda, se deberia hacer una consulta sql y usar lo de las filiminas
+    meta = comentario.meta
+    del comentario 
+    return redirect('meta_detalle', pk=meta.id)
