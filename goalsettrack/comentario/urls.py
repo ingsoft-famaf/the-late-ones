89820fa3ba2e847730910b9b31f44cd4cfdc6407
IgnaMade <ignamadevollen@gmail.com>
2016-11-07 20:35:08 -0300

    eliminar, crear,listar y editar comentarios en submeta creado

--- goalsettrack/comentario/urls.py
+++ goalsettrack/comentario/urls.py
@@ -5,12 +5,18 @@ from . import views
 urlpatterns = [
     url(r'^crear_comentario/(?P<pk>[0-9]+)$',
         views.crear_comentario, name='crear_comentario'),
-    url(r'^detalle_comentario/(?P<pk>[0-9]+)$',
-        views.detalle_comentario, name='detalle_comentario'),
     url(r'^lista_comentarios/(?P<pk>[0-9]+)$',
         views.lista_comentarios, name='lista_comentarios'),
     url(r'^eliminar_comentario/(?P<pk>[0-9]+)$',
         views.eliminar_comentario, name='eliminar_comentario'),
     url(r'^editar_comentario/(?P<pk>[0-9]+)$',
-        views.editar_comentario, name='editar_comentario')
+        views.editar_comentario, name='editar_comentario'),
+    url(r'^crear_comentario_submeta/(?P<pk>[0-9]+)$',
+        views.crear_comentario_submeta, name='crear_comentario_submeta'),
+    url(r'^eliminar_comentario_submeta/(?P<pk>[0-9]+)$',
+        views.eliminar_comentario_submeta, name='eliminar_comentario_submeta'),
+    url(r'^editar_comentario_submeta/(?P<pk>[0-9]+)$',
+        views.editar_comentario_submeta, name='editar_comentario_submeta'),
+    url(r'^lista_comentarios_submeta/(?P<pk>[0-9]+)$',
+        views.lista_comentarios_submeta, name='lista_comentarios_submeta')
 ]
--- goalsettrack/comentario/views.py
+++ goalsettrack/comentario/views.py
@@ -10,7 +10,7 @@ correcciones deberia poder funcionar.
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 
-from meta.models import Meta
+from meta.models import *
 
 from .forms import ComentarioFormulario
 from .models import Comentario
@@ -83,13 +83,66 @@ def editar_comentario(request, pk):
     return render(request, 'editar_comentario.html', {'form': form})
 
 
+##########################################################################3
+# comentario para submeta
+###########################################################################
+
 @login_required
-def detalle_comentario(request, comentario_id):
-    """
-    En otra pagina el usuario elige ver un comentario en detalle, hace click
-    en un boton, y eso postea el comentairo_id a esta funcion
-    a partir de ese id, se busca y obtiene el comentario, y se lo muestra
-    en el template detalle.html
-    """
-    comentario = get_object_or_404(Comentario, pk=comentario_id)
-    return render(request, 'detalle_comentario', {'comentario': comentario})
+def crear_comentario_submeta(request, pk):
+    """ Crea y agrega un comentario a una meta identificada por su id """
+    # meta = get_object_or_404(Meta, pk=pk)
+    meta = Submeta.objects.get(pk=pk)
+    # si ya se creo se guarda el comentario y se redirecciona el navegador a
+    # la meta
+    if request.method == "POST":
+        form = ComentarioFormulario(request.POST)
+        if form.is_valid():
+            # Se crea el comentario con los datos del formulario
+            comentario = form.save(commit=False)
+            # se lo relaciona con la meta (foreing key y eso)
+            comentario.meta = meta
+            # se guarda el comentario en la base de datos
+            comentario.save()
+            # comentarios = Comentario.objects.filter(meta__pk=pk)
+            return redirect('info_submeta', pk=meta.id)
+            # sino se crea un formulario vacio y se lo envia al template
+            # crear_comentario, para que el usuario cree el comentario
+            # cargando los datos.
+    else:
+        form = ComentarioFormulario(instance=meta)
+
+    return render(request, 'crear_comentario_submeta.html', {'form': form})
+
+@login_required
+def lista_comentarios_submeta(request, pk):
+    meta = Submeta.objects.get(pk=pk)
+    comentarios = Comentario.objects.filter(meta__pk=pk)
+    return render(request, 'lista_comentarios_submeta.html',
+                  {'meta': meta, 'comentarios': comentarios})
+
+
+@login_required
+def eliminar_comentario_submeta(request, pk):
+    comentario = Comentario.objects.get(pk=pk)
+    meta = comentario.meta
+    Comentario.objects.filter(pk=pk).delete()
+    return redirect('info_submeta', pk=meta.id)
+
+@login_required
+def editar_comentario_submeta(request, pk):
+    comentario = Comentario.objects.get(pk=pk)
+    meta = comentario.meta
+    if request.method == "POST":
+        form = ComentarioFormulario(request.POST, instance=comentario)
+        if form.is_valid():
+            # Se crea el comentario con los datos del formulario
+            comentario = form.save(commit=False)
+            # se lo relaciona con la meta
+            comentario.meta = meta
+            # se guarda el comentario en la base de datos
+            comentario.save()
+            return redirect('info_submeta', pk=meta.id)
+    else:
+        form = ComentarioFormulario(instance=comentario)
+
+    return render(request, 'editar_comentario_submeta.html', {'form': form})
