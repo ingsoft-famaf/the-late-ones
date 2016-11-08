0a2fb1f1fb2d3c592fefbedf9e7e6efab4028b98
IgnaMade <ignamadevollen@gmail.com>
2016-11-08 02:05:03 -0300

    todos los metodos de archivo adjunto para submeta, pero con error en direccionar eliminar a adjunto en submeta

--- goalsettrack/archivoadjunto/views.py
+++ goalsettrack/archivoadjunto/views.py
@@ -40,3 +40,37 @@ def eliminar_archivo(request, pk):
 
 
 
+#################################
+# SUBIR ARCHIVO A SUB META
+
+@login_required
+def subir_archivo_submeta(request, pk):
+    meta = Submeta.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = FormularioArchAdj(request.POST, request.FILES)
+        if form.is_valid():
+            archivo = form.save(commit=False)
+            archivo.meta = meta
+            archivo.save()
+            message = "Archivo subido con exito!"
+            return redirect('editar_submeta', pk=meta.pk)
+    else:
+        form = FormularioArchAdj()
+    return render(request, 'subir_archivo.html', {'form': form})
+
+@login_required
+def lista_archivos_submeta(request, pk):
+    meta = Submeta.objects.get(pk=pk)
+    archivos = ArchivoAdjunto.objects.filter(meta__pk=pk)
+    return render(request, 'lista_archivo_adj.html',
+                  {'meta': meta, 'archivos': archivos})
+
+@login_required
+def eliminar_archivo_submeta(request, pk):
+    archivo = ArchivoAdjunto.objects.get(pk=pk)
+    # se elimina archivo de server i.e directorio /archivos/ 
+    os.remove(archivo.archivo.path)
+    # se elimina archivo de la base de datos de django
+    ArchivoAdjunto.objects.filter(pk=pk).delete()
+    return redirect('lista_meta')
+
