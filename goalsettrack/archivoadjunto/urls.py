348be2344742c2b426d8f9f1f92359162afed9aa
Miguel Maldonado <miguel.alberto.maldonado@gmail.com>
2016-11-08 03:38:33 -0300

    Se resuelve merge

--- goalsettrack/archivoadjunto/urls.py
+++ goalsettrack/archivoadjunto/urls.py
@@ -1,13 +1,10 @@
 from django.conf.urls import url
-from django.views.generic import TemplateView
+
 from . import views
 
-
 urlpatterns = [
-    url(r'^subir_archivo/(?P<pk>[0-9]+)$', views.subir_archivo, name='subir_archivo'),
-    url(r'^lista_archivo_adj/(?P<pk>[0-9]+)$', views.lista_archivos, name='lista_archivos'),
-    url(r'^eliminar_archivo/(?P<pk>[0-9]+)$', views.eliminar_archivo, name='eliminar_archivo'),
-    url(r'^subir_archivo_submeta/(?P<pk>[0-9]+)$', views.subir_archivo_submeta, name='subir_archivo_submeta'),
-    url(r'^lista_archivo_adj_submeta/(?P<pk>[0-9]+)$', views.lista_archivos_submeta, name='lista_archivos_submeta'),
-    url(r'^eliminar_archivo_submeta/(?P<pk>[0-9]+)$', views.eliminar_archivo_submeta, name='eliminar_archivo_submeta'),
+    url(r'^subir_archivo/(?P<pk>[0-9]+)$',
+        views.subir_archivo, name='subir_archivo'),
+    url(r'^lista_archivo_adj/(?P<pk>[0-9]+)$',
+        views.lista_archivos, name='lista_archivos'),
 ]
--- goalsettrack/archivoadjunto/views.py
+++ goalsettrack/archivoadjunto/views.py
@@ -1,9 +1,10 @@
-import os
-from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+
+from meta.models import Meta
+
 from .forms import FormularioArchAdj
-from meta.models import *
-from .models import *
+from .models import ArchivoAdjunto
 
 
 @login_required
@@ -16,10 +17,11 @@ def subir_archivo(request, pk):
             archivo.meta = meta
             archivo.save()
             message = "Archivo subido con exito!"
-            return redirect('editar_meta', pk=meta.pk)
+            return redirect('lista_de_metas')
     else:
         form = FormularioArchAdj()
     return render(request, 'subir_archivo.html', {'form': form})
+
 
 @login_required
 def lista_archivos(request, pk):
@@ -27,50 +29,3 @@ def lista_archivos(request, pk):
     archivos = ArchivoAdjunto.objects.filter(meta__pk=pk)
     return render(request, 'lista_archivo_adj.html',
                   {'meta': meta, 'archivos': archivos})
-
-@login_required
-def eliminar_archivo(request, pk):
-    archivo = ArchivoAdjunto.objects.get(pk=pk)
-    meta = archivo.meta
-    # se elimina archivo de server i.e directorio /archivos/ 
-    os.remove(archivo.archivo.path)
-    # se elimina archivo de la base de datos de django
-    ArchivoAdjunto.objects.filter(pk=pk).delete()
-    return redirect('info_meta', pk=meta.pk)
-
-
-
-#################################
-# SUBIR ARCHIVO A SUB META
-
-@login_required
-def subir_archivo_submeta(request, pk):
-    meta = Submeta.objects.get(pk=pk)
-    if request.method == 'POST':
-        form = FormularioArchAdj(request.POST, request.FILES)
-        if form.is_valid():
-            archivo = form.save(commit=False)
-            archivo.meta = meta
-            archivo.save()
-            message = "Archivo subido con exito!"
-            return redirect('editar_submeta', pk=meta.pk)
-    else:
-        form = FormularioArchAdj()
-    return render(request, 'subir_archivo.html', {'form': form})
-
-@login_required
-def lista_archivos_submeta(request, pk):
-    meta = Submeta.objects.get(pk=pk)
-    archivos = ArchivoAdjunto.objects.filter(meta__pk=pk)
-    return render(request, 'lista_archivo_adj.html',
-                  {'meta': meta, 'archivos': archivos})
-
-@login_required
-def eliminar_archivo_submeta(request, pk):
-    archivo = ArchivoAdjunto.objects.get(pk=pk)
-    # se elimina archivo de server i.e directorio /archivos/ 
-    os.remove(archivo.archivo.path)
-    # se elimina archivo de la base de datos de django
-    ArchivoAdjunto.objects.filter(pk=pk).delete()
-    return redirect('lista_meta')
-
