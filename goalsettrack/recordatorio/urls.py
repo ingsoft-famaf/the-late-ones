fd753a1e8fa1f768bb9fd56579a630f4500c8afd
IgnaMade <ignamadevollen@gmail.com>
2016-11-08 03:28:23 -0300

    eliminar recordatorios viejos implementado

--- goalsettrack/recordatorio/urls.py
+++ goalsettrack/recordatorio/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^lista_recordatorio_meta/eliminar_recordatorio/(?P<pk>[0-9]+)$', views.eliminar_recordatorio, name='eliminar_recordatorio'),
     url(r'^lista_recordatorio_meta/editar_recordatorio/(?P<pk>[0-9]+)$', views.editar_recordatorio, name='editar_recordatorio'),
     url(r'^lista_recordatorio_meta/info_recordatorio/(?P<pk>[0-9]+)$', views.info_recordatorio, name='info_recordatorio'),
+    url(r'^lista_recordatorio_meta/eliminar_recordatorios_viejos/(?P<pk>[0-9]+)$', views.eliminar_recordatorios_viejos, name='eliminar_recordatorios_viejos'),
 ]
