3a76b5b84b54023a907516ab64f6086fe25ea3bf
IgnaMade <ignamadevollen@gmail.com>
2016-11-08 00:55:19 -0300

    metodo de eliminar archivo adjunto implementado

--- goalsettrack/archivoadjunto/urls.py
+++ goalsettrack/archivoadjunto/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     url(r'^subir_archivo/(?P<pk>[0-9]+)$', views.subir_archivo, name='subir_archivo'),
     url(r'^lista_archivo_adj/(?P<pk>[0-9]+)$', views.lista_archivos, name='lista_archivos'),
+    url(r'^eliminar_archivo/(?P<pk>[0-9]+)$', views.eliminar_archivo, name='eliminar_archivo'),
 ]
--- goalsettrack/archivoadjunto/views.py
+++ goalsettrack/archivoadjunto/views.py
@@ -1,8 +1,10 @@
+import os
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import FormularioArchAdj
 from meta.models import *
-from .models import ArchivoAdjunto
+from .models import *
+
 
 @login_required
 def subir_archivo(request, pk):
@@ -14,7 +16,7 @@ def subir_archivo(request, pk):
             archivo.meta = meta
             archivo.save()
             message = "Archivo subido con exito!"
-            return redirect('lista_de_metas')
+            return redirect('editar_meta', pk=meta.pk)
     else:
         form = FormularioArchAdj()
     return render(request, 'subir_archivo.html', {'form': form})
@@ -25,3 +27,16 @@ def lista_archivos(request, pk):
     archivos = ArchivoAdjunto.objects.filter(meta__pk=pk)
     return render(request, 'lista_archivo_adj.html',
                   {'meta': meta, 'archivos': archivos})
+
+@login_required
+def eliminar_archivo(request, pk):
+    archivo = ArchivoAdjunto.objects.get(pk=pk)
+    meta = archivo.meta
+    # se elimina archivo de server i.e directorio /archivos/ 
+    os.remove(archivo.archivo.path)
+    # se elimina archivo de la base de datos de django
+    ArchivoAdjunto.objects.filter(pk=pk).delete()
+    return redirect('info_meta', pk=meta.pk)
+
+
+
