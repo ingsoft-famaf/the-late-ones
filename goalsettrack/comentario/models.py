aa588a8ed400a22bc2907f5aa4754d2b4626fcf0
Miguel Maldonado <miguel.alberto.maldonado@gmail.com>
2016-11-14 04:44:23 -0300

    Quitando codigo muerto

--- goalsettrack/comentario/models.py
+++ goalsettrack/comentario/models.py
@@ -5,6 +5,6 @@ from meta.models import MetaAbstracta
 
 class Comentario(models.Model):
     """ cada comentarios es de una única meta / submeta """
-    texto = models.CharField(max_length=1000, default='Comentario.')
+    texto = models.CharField(max_length=1000, default='Comentario')
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     meta = models.ForeignKey(MetaAbstracta, on_delete=models.CASCADE)
